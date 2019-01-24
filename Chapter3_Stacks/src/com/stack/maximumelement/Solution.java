b2bfcdebabf4f7fcc33bd2e0f4fb8719928ea3de
meda123 <mrt62@cornell.edu>
2019-01-24 08:27:21 -0500

    Added solution for equalstacks hackerrank problem.

--- Chapter3_Stacks/src/com/stack/maximumelement/Solution.java
+++ Chapter3_Stacks/src/com/stack/maximumelement/Solution.java
@@ -4,7 +4,45 @@ package com.stack.maximumelement;
 // Push element x into stack, delete element present at the top of the stack, and print maximum element 
 // Hackerrank exercise: https://www.hackerrank.com/challenges/maximum-element/problem
 
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
+    public static void main(String[] args) {
+       Stack<Integer> stack    = new Stack<Integer>();
+       Stack<Integer> maxStack = new Stack<Integer>(); // keeps track of maximums
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        
+        for (int i = 0; i < N; i++) {
+            int query = scan.nextInt();
+            switch (query) {
+                case 1:
+                    int x = scan.nextInt();
+                    stack.push(x);
+                    if (maxStack.isEmpty() || x >= maxStack.peek()) {
+                        maxStack.push(x);
+                    }
+                    break;
+                case 2:
+                    int poppedValue = stack.pop();
+                    if (poppedValue == maxStack.peek()) {
+                        maxStack.pop();
+                    }
+                    break;
+                case 3:
+                    System.out.println(maxStack.peek());
+                    break;
+                default:
+                    break;
+            }
+        }        
+        scan.close();
+    }
 }
+
