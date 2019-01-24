a16d81859a0763c77f204199b1434b4aec0ff64c
meda123 <mrt62@cornell.edu>
2019-01-23 22:03:34 -0500

    Added hackerrank challenge

---
+++ Chapter3_Stacks/src/com/stack/maximumelement/Solution.java
@@ -0,0 +1,10 @@
+package com.stack.maximumelement;
+
+// You have an empty sequence, and you will be given  queries. Each query is one of these three types:
+// Push element x into stack, delete element present at the top of the stack, and print maximum element 
+// Hackerrank exercise: https://www.hackerrank.com/challenges/maximum-element/problem
+
+
+public class Solution {
+
+}
